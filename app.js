dcd2d468ad43d9c4ea7f9eb99ba8965e2baa5ac8
TechBitsArmy <200305105118@paruluniversity.ac.in>
2023-09-18 17:38:14 +0530

    Improved Stripe Payment

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-// const bookingController = require('./controllers/bookingController');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -52,11 +52,11 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// app.post(
-//   '/webhook-checkout',
-//   express.raw({ type: '*/*' }),
-//   bookingController.webhookCheckout
-// );
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: '*/*' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
